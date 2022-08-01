495c8f9c5c812b51b547cec40d3bcb79b572009d
mohdzaid <mohdzaid@gmail.com>
2022-08-01 17:32:38 +0530

    initial commit

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,6 @@
 // CRUD create read update delete
 
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
@@ -17,51 +16,15 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    // db.collection("users").insertOne({
-    //   name: "Andrew",
-    //   age: 27,
-    // });
-
-    // db.collection("users").insertMany(
-    //   [
-    //     {
-    //       name: "Jen",
-    //       age: 28,
-    //     },
-    //     {
-    //       name: "Gunther",
-    //       age: 27,
-    //     },
-    //   ],
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert documents");
-    //     }
-
-    //     console.log(result.ops);
-    //   }
-    // );
-    db.collection("tasks").insertMany(
-      [
-        {
-          description: "Clean the house",
-          completed: true,
-        },
-        {
-          description: "Renew inspection",
-          completed: false,
-        },
-        {
-          description: "Pot plants",
-          completed: false,
-        },
-      ],
-      (error, results) => {
-        if (error) {
-          return console.log("Unable to insert tasks !");
-        }
-        console.log(results.ops);
-      }
-    );
+    db.collection("users")
+      .find({ age: 27 })
+      .toArray((error, users) => {
+        console.log(users);
+      });
+    db.collection("users")
+      .find({ age: 27 })
+      .count((error, count) => {
+        console.log(count);
+      });
   }
 );
