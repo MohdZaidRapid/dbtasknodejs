f1e7a9e4cbef1d83e078e030b68046861a675fff
mohdzaid <mohdzaid@gmail.com>
2022-08-02 11:31:04 +0530

    initial commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,22 +1,55 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true,
 });
 
-// const User = mongoose.model("User", {
-//   name: {
-//     type: String,
-//   },
-//   age: {
-//     type: Number,
-//   },
-// });
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error(`Password cannot contain "password"`);
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
+  },
+});
 
 // const me = new User({
-//   name: "Zaid",
-//   age: "Mike",
+//   name: "      Zaid   ",
+//   email: "ZAID@MEAD.IO",
+//   password: "phone098!",
 // });
 
 // me.save()
@@ -26,3 +59,28 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
 //   .catch((error) => {
 //     console.log("Error", error);
 //   });
+
+const Task = mongoose.model("Task", {
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const task = new Task({
+  description: "  Eat lunch   ",
+});
+
+task
+  .save()
+  .then(() => {
+    console.log(task);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
