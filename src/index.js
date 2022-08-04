fa117f32e0a5b0c5365ea2551486368ef3dae487
mohdzaid <mohdzaid@gmail.com>
2022-08-04 11:51:20 +0530

    remove user and its related all task

--- src/index.js
+++ src/index.js
@@ -22,14 +22,14 @@ app.listen(port, () => {
   console.log("Server is up on port" + port);
 });
 
-const Task = require("./models/task");
-const User = require("./models/user");
-const main = async () => {
-  // const task = await Task.findById("62ea79c46c3b8059c81bde4d");
-  // await task.populate("owner").execPopulate();
-  // console.log(task.owner);
-  const user = await User.findById("62ea7590d03f61484cd98620");
-  await user.populate("tasks").execPopulate()
-  // console.log(user.tasks);
-};
-main();
+// const Task = require("./models/task");
+// const User = require("./models/user");
+// const main = async () => {
+//   // const task = await Task.findById("62ea79c46c3b8059c81bde4d");
+//   // await task.populate("owner").execPopulate();
+//   // console.log(task.owner);
+//   const user = await User.findById("62ea7590d03f61484cd98620");
+//   await user.populate("tasks").execPopulate()
+//   // console.log(user.tasks);
+// };
+// main();
