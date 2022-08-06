575e71f1e3f35a111af89d1e78be601ac9b75b2d
mohdzaid <mohdzaid@gmail.com>
2022-08-06 16:31:57 +0530

    done file upload

--- src/router/user.js
+++ src/router/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const User = require("../models/user");
 const auth = require("../middleware/auth");
+const sharp = require("sharp");
 
 const router = new express.Router();
 
@@ -120,7 +121,6 @@ router.delete("/users/me", auth, async (req, res) => {
   }
 });
 const upload = multer({
-  dest: "avatars",
   limits: {
     fileSize: 1000000,
   },
@@ -136,11 +136,38 @@ router.post(
   "/users/me/avatar",
   auth,
   upload.single("avatar"),
-  (req, res) => {
+  async (req, res) => {
+    const buffer = await sharp(req.file.buffer)
+      .resize({ widht: 250, height: 250 })
+      .png()
+      .toBuffer();
+    req.user.avatar = buffer;
+    await req.user.save();
+
     res.send();
   },
   (error, req, res, next) => {
     res.status(400).send({ error: error.message });
   }
 );
+
+router.delete("/users/me/avatar", auth, async (req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
+  res.send();
+});
+
+router.get("/users/:id/avatar", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user || !user.avatar) {
+      throw new Error();
+    }
+    res.set("Content-Type", "image/png");
+    res.send(user.avatar);
+  } catch (e) {
+    res.status(404).send();
+  }
+});
+
 module.exports = router;
