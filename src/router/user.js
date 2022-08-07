434fa83354b9db70799db8fb58c5f177aeb77bb5
mohdzaid <mohdzaid@gmail.com>
2022-08-07 16:06:41 +0530

    inital commit

--- src/router/user.js
+++ src/router/user.js
@@ -14,10 +14,9 @@ router.post("/users", async (req, res) => {
     await user.save();
     sendWelcomeEmail(user.email, user.name);
     const token = await user.generateAuthToken();
-    console.log(user);
-    res.status(201).send({ user, token });
+    res.status(201).json({ user, token });
   } catch (e) {
-    res.status(400).send(e);
+    res.status(400).send("error");
   }
 });
 
