dfb59ded48d8eaecc23276886cb1680b199eadc0
mohdzaid <mohdzaid@gmail.com>
2022-08-07 14:38:58 +0530

    fix bug

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_DB_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
